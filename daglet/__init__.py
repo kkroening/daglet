204655f95f4aa386c7a15b98450542b9e580f107
Karl Kroening <karlk@kralnet.us>
2017-07-10 03:58:06 -0600

    Update

--- daglet/__init__.py
+++ daglet/__init__.py
@@ -5,116 +5,190 @@ from builtins import object
 import copy
 
 
+def _arg_kwarg_repr(args, kwargs):
+    items = ['{!r}'.format(arg) for arg in args]
+    items += ['{}={!r}'.format(key, kwargs[key]) for key in sorted(kwargs)]
+    return ', '.join(items)
+
+
 class Edge(object):
-    """Connection to one or more nodes in a directed-acyclic graph (DAG)."""
-    def __init__(self, label, upstream_node, downstream_node, extra_hash=None):
+    """Connection to one or more vertices in a directed-acyclic graph (DAG)."""
+    def __init__(self, label, upstream_vertex, downstream_vertex=None, extra_hash=None):
         self.__label = label
-        self.__upstream_node = upstream_node
-        self.__downstream_node = downstream_node
-        self.__hash = get_hash_int([upstream_node, downstream_node, label, extra_hash])
+        self.__upstream_vertex = upstream_vertex
+        self.__downstream_vertex = downstream_vertex
+        self.__extra_hash = extra_hash
+        self.__hash = get_hash_int([label, upstream_vertex, downstream_vertex, extra_hash])
 
     @property
     def label(self):
         return self.__label
 
-    @property
-    def upstream_node(self):
-        return self.__upstream_node
+    def clone(self, **kwargs):
+        base_kwargs = {
+            'label': self.__label,
+            'upstream_vertex': self.__upstream_vertex,
+            'downstream_vertex': self._downstream_vertex,
+            'extra_hash': self.__extra_hash,
+        }
+        base_kwargs.update(kwargs)
+        return Edge(**base_kwargs)
 
     @property
-    def downstream_node(self):
-        return self.__downstream_node
+    def upstream_vertex(self):
+        return self.__upstream_vertex
+
+    @property
+    def downstream_vertex(self):
+        return self.__downstream_vertex
 
     def __hash__(self):
         return self.__hash
 
     def __eq__(self, other):
-        raise hash(self) == hash(other)
+        return hash(self) == hash(other)
+
+    @property
+    def short_repr(self):
+        return 'daglet.Edge({!r})'.format(self.__label)
+
+    @property
+    def short_hash(self):
+        return '{:x}'.format(abs(hash(self)))[:12]
+
+    def __repr__(self):
+        return '{} <{}>'.format(self.short_repr, self.short_hash)
+
+    @property
+    def upstream_index(self):
+        if self.__upstream_vertex is not None:
+            index = self.__upstream_vertex.get_incoming_edge_index(self)
+        else:
+            index = None
+        return index
+
+    @property
+    def downstream_index(self):
+        if self.__downstream_vertex is not None:
+            index = self.__downstream_vertex.get_incoming_edge_index(self)
+        else:
+            index = None
+        return index
+
+    def vertex(self, label, extra_hash=None):
+        return Vertex(label, extra_hash, [self])
+
+    def reverse(self):
+        return Edge(self.__label, self.__downstream_vertex, self.__upstream_vertex, self.__extra_hash)
 
 
-
-class Node(object):
-    """Node in a directed-acyclic graph (DAG).
-
-    Edges:
-        Nodes are connected by edges.  An edge connects two nodes with a label for each side:
-         - ``upstream_node``: upstream/outgoing/parent ``Node``
-         - ``downstream_node``: downstream/incoming/child ``Node``
-         - ``label``: label ``string``
-         - ``
-
-        For example, Node A may be connected to Node B with an edge labelled "foo":
-
-           _____             _____
-          |     |           |     |
-          |  A  >---[foo]--->  B  |
-          |_____|           |_____|
-
-        Edge labels may be integers or strings, and nodes cannot have more than one incoming edge with the same label.
-
-        Nodes may have any number of incoming edges and any number of outgoing edges.  Nodes keep track only of
-        their incoming edges, but the entire graph structure can be inferred by looking at the furthest downstream
-        nodes and working backwards.
+class Vertex(object):
+    """Vertex in a directed-acyclic graph (DAG).
 
     Hashing:
-        Nodes must be hashable, and two nodes are considered to be equivalent if they have the same hash value.
+        Vertices must be hashable, and two vertices are considered to be equivalent if they have the same hash value.
 
-        Nodes are immutable, and the hash should remain constant as a result.  If a node with new contents is required,
-        create a new node and throw the old one away.
-
-    String representation:
-        In order for graph visualization tools to show useful information, nodes must be representable as strings.  The
-        ``repr`` operator should provide a more or less "full" representation of the node, and the ``short_repr``
-        property should be a shortened, concise representation.
-
-        Again, because nodes are immutable, the string representations should remain constant.
+        Vertices are immutable, and the hash should remain constant as a result.  If a vertex with new contents is
+        required, create a new vertex and throw the old one away.
     """
-    def __init__(self, label, incoming_edges=None, extra_hash=None):
-        self.__incoming_edges = incoming_edges
+    def __init__(self, label, edges=[], extra_hash=None):
+        self.__edges = edges
         self.__label = label
-        self.__hash = get_hash_int([label, incoming_edges, extra_hash])
+        self.__extra_hash = extra_hash
+        self.__hash = get_hash_int([label, edges, extra_hash])
+
+    @property
+    def edges(self):
+        return self.__edges
+
+    @property
+    def upstream_edges(self):
+        return [x for x in self.__edges if x.upstream_vertex == self]
+
+    @property
+    def downstream_edges(self):
+        return [x for x in self.__edges if x.downstream_vertex == self]
 
     @property
     def label(self):
         return self.__label
 
     @property
-    def upstream_node(self):
-        return self.__upstream_node
-
-    @property
-    def downstream_node(self):
-        return self.__downstream_node
+    def extra_hash(self):
+        return self.__extra_hash
 
     def __hash__(self):
         return self.__hash
 
     def __eq__(self, other):
-        raise hash(self) == hash(other)
+        return hash(self) == hash(other)
+
+    def clone(self, **kwargs):
+        base_kwargs = {
+            'label': self.__label,
+            'edges': self.__edges,
+            'extra_hash': self.__extra_hash,
+        }
+        base_kwargs.update(kwargs)
+        return Vertex(**base_kwargs)
+
+    def transplant(self, new_edges):
+        """Create a copy of this Vertex with new edges."""
+        return Vertex(self.__label, new_edges, self.__extra_hash)
+
+    def edge(self, label=None):
+        """Create edge with specified label.
+
+        Example:
+            The following example creates a DAG with three vertices connected with two edges (``n1 -> n2 -> n3``):
+            ```
+            n3 = (daglet
+                .Vertex('n1').edge('e1')
+                .vertex('n2').edge('e2')
+                .vertex('n3')
+            )
+            ```
+        """
+        return Edge(label, self)
+
+    @property
+    def short_repr(self):
+        args = [self.__label]
+        kwargs = {}
+        if self.__extra_hash is not None:
+            kwargs['extra_hash'] = self.__extra_hash
+        return 'daglet.Vertex({})'.format(_arg_kwarg_repr(args, kwargs))
+
+    @property
+    def short_hash(self):
+        return '{:x}'.format(abs(hash(self)))[:12]
+
+    def __repr__(self):
+        return '{} <{}>'.format(self.short_repr, self.short_hash)
 
 
-def topo_sort(edges):
-    marked_nodes = []
-    sorted_nodes = []
-    outgoing_edges_map = {}
+def sort(edges):
+    marked_vertices = []
+    sorted_vertices = []
+    node_edges_map = {}
 
     def visit(edge):
-        if edge.upstream_node in marked_nodes:
+        if edge.upstream_vertex in marked_vertices:
             raise RuntimeError('Graph is not a DAG')
 
-        outgoing_edges = outgoing_edges_map.get(edge.upstream_node, [])
-        outgoing_edges.append(edge)
-        outgoing_edges[edge.upstream_node] = outgoing_edges
+        node_edges = node_edges_map.get(edge.upstream_vertex, [])
+        node_edges.append(edge)
+        node_edges_map[edge.upstream_vertex] = node_edges
 
-        if edge.upstream_node not in sorted_nodes:
-            marked_nodes.append(edge.upstream_node)
-            for incoming_edge in edge.upstream_node.incoming_edges:
-                visit(incoming_edge)
-            marked_nodes.remove(edge.upstream_node)
-            sorted_nodes.append(edge.upstream_node)
+        if edge.upstream_vertex not in sorted_vertices:
+            marked_vertices.append(edge.upstream_vertex)
+            for edge in edge.upstream_vertex.edges:
+                visit(edge)
+            marked_vertices.remove(edge.upstream_vertex)
+            sorted_vertices.append(edge.upstream_vertex)
 
     unvisited_edges = copy.copy(edges)
     while unvisited_edges:
         edge = unvisited_edges.pop()
         visit(edge)
-    return sorted_nodes, outgoing_edges_map
+    return sorted_vertices, node_edges_map
--- tests/test_daglet.py
+++ tests/test_daglet.py
@@ -3,5 +3,155 @@ from __future__ import unicode_literals
 import daglet
 
 
-def test_dalget():
-    daglet.Node('test')
+def test_edge_eq_no_vertices_label_mismatch():
+    assert daglet.Edge('e1', None) != daglet.Edge('e2', None)
+
+
+def test_edge_eq_no_vertices_extra_hash_match():
+    assert daglet.Edge('e1', None, extra_hash=1) == daglet.Edge('e1', None, extra_hash=1)
+
+
+def test_edge_eq_no_vertices_extra_hash_mismatch():
+    assert daglet.Edge('e1', None, extra_hash=1) != daglet.Edge('e1', None, extra_hash=2)
+
+
+def test_edge_eq_fully_connected_same():
+    assert daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2')) == \
+        daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2'))
+
+
+def test_edge_eq_fully_connected_label_mismatch():
+    assert daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2')) != \
+        daglet.Edge('e2', daglet.Vertex('v1'), daglet.Vertex('v2'))
+
+
+def test_edge_eq_fully_upstream_vertex_mismatch():
+    assert daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2')) != \
+        daglet.Edge('e1', daglet.Vertex('v0'), daglet.Vertex('v2'))
+
+
+def test_edge_eq_fully_downstream_vertex_mismatch():
+    assert daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2')) != \
+        daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v3'))
+
+
+def test_edge_eq_same():
+    assert daglet.Vertex('v1').edge('e1') == daglet.Vertex('v1').edge('e1')
+
+
+def test_edge_eq_label_mismatch():
+    assert daglet.Vertex('v1').edge('e1') != daglet.Vertex('v1').edge('e2')
+
+
+def test_edge_eq_upstream_mismatch():
+    assert daglet.Vertex('v1').edge('e1') != daglet.Vertex('v1').edge('e2')
+
+
+def test_edge_eq_reverse():
+    assert daglet.Vertex('v1').edge('e1').reverse() == daglet.Vertex('v1').edge('e1').reverse()
+
+
+def test_edge_eq_reverse_reverse():
+    assert daglet.Vertex('v1').edge('e1').reverse().reverse() == daglet.Vertex('v1').edge('e1')
+
+
+def test_vertex_eq_no_edges_same():
+    assert daglet.Vertex('v1') == daglet.Vertex('v1')
+
+
+def test_vertex_eq_no_edges_label_mismatch():
+    assert daglet.Vertex('v1') != daglet.Vertex('v2')
+
+
+def test_vertex_eq_no_edges_extra_hash_match():
+    assert daglet.Vertex('v1', extra_hash=1) == daglet.Vertex('v1', extra_hash=1)
+
+
+def test_vertex_eq_no_edges_extra_hash_mismatch():
+    assert daglet.Vertex('v1', extra_hash=1) != daglet.Vertex('v1', extra_hash=2)
+
+
+def test_edge_eq_no_vertices_same():
+    assert daglet.Edge('e1', None) == daglet.Edge('e1', None)
+
+
+def test_vertex_eq_same_one_incoming_edge():
+    assert daglet.Vertex('v1').edge('e1').vertex('v2') == daglet.Vertex('v1').edge('e1').vertex('v2')
+
+
+def test_vertex_eq_multi_incoming_same():
+    v3a = (daglet.Vertex('v3', [
+        daglet.Vertex('v1').edge('e1'),
+        daglet.Vertex('v2').edge('e2')
+    ]))
+    v3b = (daglet.Vertex('v3', [
+        daglet.Vertex('v1').edge('e1'),
+        daglet.Vertex('v2').edge('e2')
+    ]))
+    assert v3a == v3b
+
+
+def test_vertex_eq_multi_incoming_wrong_order():
+    v3a = (daglet.Vertex('v3', [
+        daglet.Vertex('v1').edge('e1'),
+        daglet.Vertex('v2').edge('e2')
+    ]))
+    v3b = (daglet.Vertex('v3', [
+        daglet.Vertex('v2').edge('e2'),
+        daglet.Vertex('v1').edge('e1')
+    ]))
+    assert v3a != v3b
+
+
+def test_vertex_label_mismatch():
+    assert daglet.Vertex('v1').edge('e1').vertex('v2') != daglet.Vertex('v1').edge('e1').vertex('v3')
+
+
+def test_vertex_edge_mismatch():
+    assert daglet.Vertex('v1').edge('e1').vertex('v2') != daglet.Vertex('v1').edge('e2').vertex('v2')
+
+
+def test_vertex_reversed_edge_match():
+    assert daglet.Vertex('v1').edge('e1').reverse().vertex('v2') != \
+        daglet.Vertex('v1').edge('e1').reverse().vertex('v2')
+
+
+def test_vertex_reversed_edge_mismatch():
+    assert daglet.Vertex('v1').edge('e1').reverse().vertex('v2') != \
+        daglet.Vertex('v1').edge('e2').reverse().vertex('v2')
+
+
+def test_vertex_upstream_edges():
+    daglet.Edge('e1', daglet.Vertex('v1'), daglet.Vertex('v2'))
+    e1 = daglet.Vertex('v1').edge('e1')
+    e2 = daglet.Vertex('v3').edge('e2').reverse()
+    v2 = daglet.Vertex('v2', [e1, e2])
+    assert v2.upstream_edges == [e1]
+    assert v2.downstream_edges == [e2]
+
+
+def test_vertex_downstream_edges():
+    pass  # FIXME
+
+
+def test_vertex_clone():
+    assert daglet.Vertex('v1').clone(label='v2').label == 'v2'
+
+
+def test_vertex_transplant():
+    v2 = daglet.Vertex('v2')
+    assert daglet.Vertex('v1').transplant([v2]).edges == [v2]
+
+
+def test_vertex_short_repr():
+    assert daglet.Vertex('v1').short_repr == "daglet.Vertex({!r})".format('v1')
+
+
+def test_vertex_short_hash():
+    short_hash = daglet.Vertex('v1').short_hash
+    assert int(short_hash, base=16)
+
+
+def test_vertex_repr():
+    v1 = daglet.Vertex('v1')
+    assert repr(v1) == '{} <{}>'.format(v1.short_repr, v1.short_hash)
