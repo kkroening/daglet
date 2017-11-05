357d372cc7dd46e77255191c78e46854fe2ecfb1
Karl Kroening <karlk@kralnet.us>
2017-11-04 22:17:24 -0700

    Add `render` function

--- daglet/view.py
+++ daglet/view.py
@@ -5,21 +5,21 @@ import daglet
 import tempfile
 
 
-def view(objs, parent_func, filename=None, rankdir='LR', vertex_color_func={}.get,
-        vertex_label_func={}.get, edge_label_func={}.get):
+def __import_graphviz():
     try:
         import graphviz
     except ImportError:
         raise ImportError('failed to import graphviz; please make sure graphviz is installed (e.g. `pip install '
             'graphviz`)')
+    return graphviz
 
-    if filename is None:
-        filename = tempfile.mktemp()
 
-    sorted_objs = daglet.toposort(objs, parent_func)
+def __make_graph(objs, parent_func, rankdir, vertex_color_func, vertex_label_func, edge_label_func):
+    graphviz = __import_graphviz()
     graph = graphviz.Digraph()
     graph.attr(rankdir=rankdir)
 
+    sorted_objs = daglet.toposort(objs, parent_func)
     for child in sorted_objs:
         id = str(hash(child))
         label = vertex_label_func(child)
@@ -35,4 +35,22 @@ def view(objs, parent_func, filename=None, rankdir='LR', vertex_color_func={}.ge
             downstream_obj_id = str(hash(child))
             graph.edge(upstream_obj_id, downstream_obj_id, **kwargs)
 
+    return graph
+
+
+def render(objs, parent_func, filename=None, rankdir='LR', vertex_color_func={}.get, vertex_label_func={}.get,
+        edge_label_func={}.get):
+    graph = __make_graph(objs, parent_func, rankdir, vertex_color_func, vertex_label_func, edge_label_func)
+    if filename is None:
+        filename = tempfile.mktemp()
+    graph.render(filename)
+    return filename
+
+
+def view(objs, parent_func, filename=None, rankdir='LR', vertex_color_func={}.get, vertex_label_func={}.get,
+        edge_label_func={}.get):
+    graph = __make_graph(objs, parent_func, rankdir, vertex_color_func, vertex_label_func, edge_label_func)
+    if filename is None:
+        filename = tempfile.mktemp()
     graph.view(filename)
+    return filename
